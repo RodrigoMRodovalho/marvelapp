f3d42f452dce1c800d436e5f003e363aeed260db
Rodrigo Rodovalho <rodrigomrodovalho@gmail.com>
2020-10-05 02:07:23 -0300

    creating base files

---
+++ data/src/main/kotlin/br/com/rrodovalho/data/repository/MarvelRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package br.com.rrodovalho.data.repository
+
+import br.com.rrodovalho.domain.repository.MarvelRepository
+
+class MarvelRepositoryImpl: MarvelRepository{
+}
---
+++ data/src/main/kotlin/br/com/rrodovalho/data/repository/remote/api/ApiContract.kt
@@ -0,0 +1,4 @@
+package br.com.rrodovalho.data.repository.remote.api
+
+interface ApiContract {
+}
