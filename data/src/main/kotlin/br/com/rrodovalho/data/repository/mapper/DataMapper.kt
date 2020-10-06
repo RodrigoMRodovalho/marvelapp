a1be8ff54d94c2337d0fb4145a17ff0ded9d10c0
Rodrigo Rodovalho <rodrigomrodovalho@gmail.com>
2020-10-06 13:42:46 -0300

    improving datamapper code

--- data/src/main/kotlin/br/com/rrodovalho/data/repository/mapper/DataMapper.kt
+++ data/src/main/kotlin/br/com/rrodovalho/data/repository/mapper/DataMapper.kt
@@ -2,10 +2,14 @@ package br.com.rrodovalho.data.repository.mapper
 
 import br.com.rrodovalho.data.repository.remote.model.CharacterApiResponse
 import br.com.rrodovalho.data.repository.remote.model.ComicsApiResponse
+import br.com.rrodovalho.data.repository.remote.model.Thumbnail
 import br.com.rrodovalho.domain.model.Character
 import br.com.rrodovalho.domain.model.CharacterDetail
 import br.com.rrodovalho.domain.model.Comics
 import br.com.rrodovalho.domain.model.ComicsDetail
+
+const val HTTPS_REF = "https"
+const val HTTP_REF = "http"
 
 fun transformTo(characterApiResponse: CharacterApiResponse): List<Character> {
 
@@ -16,14 +20,12 @@ fun transformTo(characterApiResponse: CharacterApiResponse): List<Character> {
 
         it.comics.items.forEach { comics ->
             comicsList.add(Comics(comics.name,
-                comics.resourceURI.replace("http", "https")))
+                handleMissingHttpSecurityUrl(comics.resourceURI)))
         }
 
         val character = Character("${it.id}", it.name,
             it.description,
-            it.thumbnail.path.replace("http", "https")
-                .plus(".")
-                .plus(it.thumbnail.extension),
+            composeImageUrl(it.thumbnail),
             comicsList)
 
         characterList.add(character)
@@ -42,12 +44,30 @@ fun transform(characterApiResponse: CharacterApiResponse): CharacterDetail {
 fun transform(comicsApiResponse: ComicsApiResponse): ComicsDetail {
 
     val comicsResult = comicsApiResponse.data.results[0]
+
     return ComicsDetail(
         Comics(comicsResult.title,
         comicsResult.resourceURI),
         comicsResult.description,
-        comicsResult.thumbnail.path.replace("http", "https")
-            .plus(".")
-            .plus(comicsResult.thumbnail.extension)
+        composeImageUrl(comicsResult.thumbnail)
     )
+}
+
+private fun composeImageUrl(thumbnail: Thumbnail): String {
+    return composeImageUrlWithFileExtension(
+        handleMissingHttpSecurityUrl(thumbnail.path),
+        thumbnail.extension
+    )
+}
+
+private fun handleMissingHttpSecurityUrl(url: String): String {
+    if (url.contains(HTTPS_REF).not()){
+        return url.replace(HTTP_REF, HTTPS_REF)
+    }
+    return url
+}
+
+private fun composeImageUrlWithFileExtension(imageUrl: String, extensionFile: String): String {
+    return imageUrl.plus(".")
+        .plus(extensionFile)
 }
